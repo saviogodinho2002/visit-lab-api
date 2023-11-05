9f3df8ccbe4b0fe4ef32c90648a371390af38df4
saviogodinho2002 <saviogmoiagaia.2002@gmail.com>
2023-11-05 18:16:00 -0300

    email via back

--- app/Http/Controllers/Api/PreRegistrationController.php
+++ app/Http/Controllers/Api/PreRegistrationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\RequestSIGAA;
 use App\Http\Requests\StorePreRegistrationRequest;
 use App\Http\Requests\UpdatePreRegistrationRequest;
 use App\Models\PreRegistration;
@@ -26,22 +27,96 @@ class PreRegistrationController extends Controller
         //
     }
 
+    /**
+     * Cria um pré registro de usuário
+     *
+     *
+     * @OA\Post(
+     *      tags={"Pre registro"},
+     *     path="api/pre-registration/store",
+     *     description="Cria um pre registro para proximo login de um usuário",
+     *         security={ {"bearerToken":{}} },
+     *     @OA\RequestBody(
+     *         description="Json informações necessárias",
+     *         required=true,
+     *      @OA\MediaType(
+     *           mediaType="application/json",
+     *
+     *     @OA\Schema(
+     *      type="object",
+     *                 required={
+     *     "login",
+    *       "role_id",
+     *
+     *
+     * },
+     *     @OA\Property (
+     *         property="login",
+     *         description="Login do usuario",
+     *          type="string"
+     *     ),
+
+     *     @OA\Property (
+     *          property="role_id",
+     *          description="Role que será atribuída a um usuário",
+     *           type="number"
+     *      ),
+
+     *     @OA\Property(
+     *         property="laboratory_id",
+     *
+     *         description="Laboratório que será atribuído do usuario",
+     *           type="number"
+     *     ),
+
+     *
+     *
+     *     ),
+     * ),
+     * ),
+     *  @OA\Response(
+     *    response=200,
+     *    description="Registro criado",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="id", type="string"),
+     *    )
+     *  ),
+     *     @OA\Response(
+     *    response=404,
+     *    description="Login não encontrado",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string"),
+     *
+     *    )
+     *  )
+     *
+     * )
+     */
 
     public function store(Request $request)
     {
         $validated = $request->validate(
             [
                 "login"=>["required"],
-                "email"=>["required"],
+
                 "role_id"=>["required"],
                 "laboratory_id"=>[]
             ]
         );
-        $register  =  PreRegistration::create(
-           $validated
-        );
+        try {
+            $info = RequestSIGAA::get_info_user($validated ["login"]);
+            $validated["user_id"] = $request->user()->id;
+            $validated["email"] = $info["email"];
+            $register  =  PreRegistration::create(
+                $validated
+            );
 
-       return response()->json($register);
+            return response()->json($register);
+        }catch (\Throwable $e){
+            return response()->json("Login não encontrado",403);
+
+        }
+
     }
 
 
